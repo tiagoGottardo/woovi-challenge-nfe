7f2672a4a9ce1f0e014118344030e0516a794514
tiagoGottardo <tiago.panizio@hotmail.com>
2025-07-17 23:00:50 -0300

    chore: remove unused nfe certs

--- apps/nfe/src/server.ts
+++ apps/nfe/src/server.ts
@@ -1,19 +1,14 @@
 import path from "path"
-import fs, { readFileSync } from 'fs'
+import fs from 'fs'
 import * as https from 'https'
 import * as soap from 'soap'
 
 import { webServices } from "./services/"
 import app from "./app"
 
-const PORT = 3000
-
-const key = readFileSync(path.resolve(__dirname, '../certs/server.key'));
-const cert = readFileSync(path.resolve(__dirname, '../certs/server.crt'));
-const ca = readFileSync(path.resolve(__dirname, '../certs/ca.crt'));
+const PORT = process.env.PORT || 3000
 
 const httpsOptions: https.ServerOptions = {
-  key, cert, ca,
   requestCert: true,
   rejectUnauthorized: false,
 };
@@ -23,7 +18,7 @@ const httpServer = https.createServer(httpsOptions, app.callback());
 process.chdir(path.join(__dirname, '..', 'xsd'))
 
 webServices.forEach(ws => {
-  const xml = fs.readFileSync(path.resolve(__dirname, `../wsdl/${ws.name}.wsdl`), 'utf8');
+  const xml = fs.readFileSync(path.resolve(__dirname, `../wsdl/${ws.name}.wsdl`), 'utf8')
 
   soap.listen(httpServer, `/ws/${ws.name}`, ws.service, xml, (err, _) => {
     if (err) return console.log(err)
