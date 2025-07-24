d59aa5df1eb2c80fc6d800708f7f551a4562ded8
tiagoGottardo <tiago.panizio@hotmail.com>
2025-07-24 10:32:54 -0300

    fix: add mock certs on sefaz

--- apps/sefaz-mock/src/server.ts
+++ apps/sefaz-mock/src/server.ts
@@ -6,23 +6,30 @@ import * as soap from 'soap'
 import { webServices } from "./services/"
 import app from "./app"
 
-const PORT = process.env.SEFAZ_PORT || 3000
+const HOST = process.env.SEFAZ_HOST || 'localhost'
+const PORT = parseInt(process.env.SEFAZ_PORT || '3000')
+
+console.table({ HOST, PORT })
 
 const httpsOptions: https.ServerOptions = {
+  key: readFileSync(resolve(__dirname, '../certs/key.pem')),
+  cert: readFileSync(resolve(__dirname, '../certs/cert.pem')),
   requestCert: true,
   rejectUnauthorized: false,
 };
 
-const httpServer = https.createServer(httpsOptions, app.callback());
+const httpsServer = https.createServer(httpsOptions, app.callback());
 
 process.chdir(join(__dirname, '..', 'xsd'))
 
 webServices.forEach(ws => {
   const xml = readFileSync(resolve(__dirname, `../wsdl/${ws.name}.wsdl`), 'utf8')
 
-  soap.listen(httpServer, `/ws/${ws.name}`, ws.service, xml, (err, _) => {
+  soap.listen(httpsServer, `/ws/${ws.name}`, ws.service, xml, (err, _) => {
     if (err) return console.log(err)
   });
 })
 
-httpServer.listen(PORT, () => console.log(`Server running on https://localhost:${PORT}`))
+httpsServer.listen(PORT, HOST, () => console.log(`Server running on https://${HOST}:${PORT}`))
+
+httpsServer.on('error', e => console.error(e))
